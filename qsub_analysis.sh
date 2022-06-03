822b6954995fac1689f8e569ec89316d1c778d34
Tomas Byland <tomas.bylund@lnu.se>
2022-06-03 15:13:12 +0200

    Added call to spectrum fit and report generation

--- qsub_analysis.sh
+++ qsub_analysis.sh
@@ -5,9 +5,15 @@ condadir=$2
 condaenv=$3
 srcname=$4
 
+startdate=$(date --iso)
+
 cd $rootdir
 source $condadir
 conda activate $condaenv
 conffile=$(printf "%s/gp_%s.yaml" "$srcname" "$srcname")
+outdir=$(printf "%s/GP_Result" "$srcname")
+reportname=$(printf "%s-%s" "$srcname" "$startdate")
 printf "Running analysis %s" "$conffile"
-python gplib/OnOff-Analysis.py $conffile
+python gplib/OnOff-Analysis.py "$conffile" --debug
+python gplib/SpectrumFit.py "$conffile"
+python gplib/MakeGPReport.py "$outdir" "$reportname"
