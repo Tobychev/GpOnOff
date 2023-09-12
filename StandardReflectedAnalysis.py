950db1843995088e034ef7fcd94649ea4d143bb9
Tomas Byland <tomas.bylund@lnu.se>
2023-09-12 19:07:32 +0200

    Added support for hap-fr data

--- StandardReflectedAnalysis.py
+++ StandardReflectedAnalysis.py
@@ -51,7 +51,6 @@ def get_listed_observations(data_dir,run_list):
    return obs_list,props
 
 
-
 def get_listed_hap_observations(runlist,cut_config,prod="fits_prod05"):
    root_dir = pt.Path("/home/hfm/hess/fits/hap-hd/"+ prod)
 
@@ -62,7 +61,6 @@ def get_listed_hap_observations(runlist,cut_config,prod="fits_prod05"):
       if len(lists[key]) > 0:
          data_dir = root_dir / key / cut_config
          obs[key] = get_listed_observations(data_dir, lists[key]) 
-
 
    return obs
 
@@ -76,6 +74,7 @@ def get_listed_hd_fr_observations(runlist,cut_config,prod="Prod23_Calib0834"):
    
    data_dir = pt.Path("/home/hfm/hess/fits/hap-fr/") / prod / cut_config
    obs["hap-fr"] = get_listed_observations(data_dir, runs)
+   return obs
 
 # Start of MAIN
 if __name__ == "__main__":
@@ -90,14 +89,16 @@ if __name__ == "__main__":
     src_ana_conf, src_pos, conf = ut.make_Analysis_config_from_yaml(args.config)
     ut.check_paths(conf)
     rebin = conf["optional"].get("fit_energy_bins",None)
-    root_dir = pt.Path("/home/hfm/hess/fits/hap-hd/") / conf["optional"]["production"] 
+    root_dir = pt.Path(conf["data_directory"]) / conf["optional"]["production"] 
     out_dir = pt.Path(conf["out_path"])
 
     if "hap-hd" in conf["data_directory"]:
        obs_dict = get_listed_hap_observations(conf["optional"]["runlist"],conf["optional"]["cut_conf"])
+       datastore = "{root_dir}/{key}/{cut_conf}"
 
     if "hap-fr" in conf["data_directory"]:
        obs_dict = get_listed_hd_fr_observations(conf["optional"]["runlist"],conf["optional"]["cut_conf"])
+       datastore = "{root_dir}/{cut_conf}"
 
     for key in obs_dict:
         obs_objs, obs_tab = obs_dict[key]
@@ -108,7 +109,12 @@ if __name__ == "__main__":
         ana_conf.general.datasets_file = out_loc / f"{conf['source']}_{key}_dataset.yaml" 
 
         ana_conf.observations.obs_ids = [itm.obs_id for itm in obs_objs]
-        ana_conf.observations.datastore = root_dir / key / conf["optional"]["cut_conf"]
+        ana_conf.observations.datastore = pt.Path(
+           datastore.format(
+              root_dir= root_dir,
+              key= key,
+              cut_conf = conf["optional"]["cut_conf"]))
+
         ana_conf.observations.required_irf = ["aeff","edisp","psf", "bkg"]
 
         ana_conf.datasets.map_selection = ["counts","exposure","edisp", "background"]
